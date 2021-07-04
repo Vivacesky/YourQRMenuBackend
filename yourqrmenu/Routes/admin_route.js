582a684076564f6398a48dd644a5930bf3f17919
furkanpinar <furkanpinar9@hotmail.com>
2021-07-04 11:38:09 +0200

    cors fix, route fit

--- yourqrmenu/Routes/admin_route.js
+++ yourqrmenu/Routes/admin_route.js
@@ -3,7 +3,7 @@ const companyController = require('../Controllers/company_controller');
 
 const router = express.Router({mergeParams: true});
 
-router.post('/', companyController.addCompany);
+router.post('/company', companyController.addCompany);
 router.delete('/company/:companyId', companyController.deleteCompany);
 router.patch('/company', companyController.updateCompany);
 
